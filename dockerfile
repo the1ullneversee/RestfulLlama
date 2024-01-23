5ed587b1cc6e7f6ab97bb869a548eb8fa5b6cbe6
Tom <tkk100@msn.com>
2024-01-23 19:10:27 +0000

    changes to dockerfile and finetuner file

--- dockerfile
+++ dockerfile
@@ -1,38 +1,27 @@
-ARG PYTHON_VERSION="3.10"
-ARG BUILD_VARIANT="bullseye"
-ARG RUNTIME_VARIANT="slim-bullseye"
-
 FROM nvidia/cuda:11.0.3-base-ubuntu20.04
-FROM python:${PYTHON_VERSION}-${BUILD_VARIANT}
 # Set environment variables
 ENV DEBIAN_FRONTEND=noninteractive
-
 # Install system dependencies
 RUN apt-get update && \
     apt-get install -y \
-        git \
-        python3-pip \
-        python3-dev \
-        python3-opencv \
-        libglib2.0-0
-# Install any python packages you need
+        python3-pip
+
+RUN apt-get update && \
+    apt-get install -y software-properties-common && \
+    add-apt-repository -y ppa:deadsnakes/ppa && \
+    apt-get update && \
+    apt install -y python3.10
+
 
 WORKDIR /code
 RUN pip install poetry
-
 COPY ./pyproject.toml ./poetry.lock* /code/
 COPY . /code
-
-RUN poetry config virtualenvs.in-project true
-
-WORKDIR /code
-
-RUN poetry source add -p explicit pytorch https://download.pytorch.org/whl/cu111/torch_stable.html
-RUN poetry add torch torchvision torchaudio
+RUN poetry config virtualenvs.in-project false
+RUN poetry env use /usr/bin/python3.10
 RUN poetry install
 RUN poetry run pip install torch==2.0.1 torchvision==0.15.2 torchaudio==2.0.2
-
-
+ENV PATH=/code/.venv/bin:$PATH
 
 RUN echo 'export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/conda/lib/libcudart.so' >> ~/.bashrc 
 
@@ -49,8 +38,5 @@ RUN sed 's@session\s*required\s*pam_loginuid.so@session optional pam_loginuid.so
 
 ENV NOTVISIBLE "in users profile"
 RUN echo "export VISIBLE=now" >> /etc/profile
-
 EXPOSE 22
 CMD ["/usr/sbin/sshd", "-D"]
-
-WORKDIR /code
--- fine_tuner.py
+++ fine_tuner.py
@@ -1,61 +1,162 @@
-import os, sys
 import torch
 from transformers import (
     AutoTokenizer,
-    AutoModelForCausalLM,
     BitsAndBytesConfig,
-    DataCollatorForLanguageModeling,
-    DataCollatorForSeq2Seq,
-    Trainer,
     TrainingArguments,
-    GenerationConfig
 )
-#from peft import PeftModel, LoraConfig, prepare_model_for_kbit_training, get_peft_model
+from peft import PeftModel, LoraConfig, prepare_model_for_kbit_training, get_peft_model
 
-model_id='NousResearch/Llama-2-7b-hf'
+model_id='NousResearch/Llama-2-13b-hf'
 max_length = 512
 device_map = "auto"
 batch_size = 128
 micro_batch_size = 32
 gradient_accumulation_steps = batch_size // micro_batch_size
 
-# nf4" use a symmetric quantization scheme with 4 bits precision
-# bnb_config = BitsAndBytesConfig(
-#     load_in_4bit=True,
-#     bnb_4bit_use_double_quant=True,
-#     bnb_4bit_quant_type="nf4",
-#     bnb_4bit_compute_dtype=torch.bfloat16,
-#     load_in_8bit_fp32_cpu_offload=True,
-# )
+from transformers import AutoModelForCausalLM
+compute_dtype = getattr(torch, "float16")
+
+quant_config = BitsAndBytesConfig(
+    load_in_4bit=True,
+    bnb_4bit_quant_type="nf4",
+    bnb_4bit_compute_dtype=compute_dtype,
+    bnb_4bit_use_double_quant=False,
+)
 
 # load model from huggingface
-# model = AutoModelForCausalLM.from_pretrained(
-#     model_id,
-#     quantization_config=bnb_config,
-#     use_cache=False,
-#     device_map=device_map
-# )
-
-from transformers import AutoModelForCausalLM
 model = AutoModelForCausalLM.from_pretrained(
-  device_map='auto',
-  load_in_8bit=True,
-  pretrained_model_name_or_path='NousResearch/Llama-2-7b-hf',
-  max_memory=f'{int(torch.cuda.mem_get_info()[0]/1024**3)-2}GB')
+    model_id,
+    quantization_config=quant_config,
+    use_cache=False,
+    device_map=device_map
+)
+model.config.use_cache = False
+model.config.pretraining_tp = 1
 
+
+from datasets import load_dataset
+data_files = {"train": "../train_dataset.jsonl", "test": "../test_dataset.jsonl"}
+
+main_dataset = load_dataset('json', data_files=data_files)
+test, train = main_dataset['test'], main_dataset['train']
 # load tokenizer from huggingface
-tokenizer = AutoTokenizer.from_pretrained(model_id)
+tokenizer = AutoTokenizer.from_pretrained("TheBloke/Llama-2-13B-chat-GPTQ", token="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")
 tokenizer.pad_token = tokenizer.eos_token
 tokenizer.padding_side = "right"
 
-# LoRA config
-model = prepare_model_for_kbit_training(model)
-peft_config = LoraConfig(
-    r=8,
-    lora_alpha=32,
+peft_params = LoraConfig(
+    lora_alpha=16,
     lora_dropout=0.1,
-    target_modules=["q_proj", "v_proj"],
+    r=64,
     bias="none",
+    target_modules=["q_proj", "k_proj","v_proj","o_proj"], # the name of the layers to add LoRA
     task_type="CAUSAL_LM",
+    modules_to_save=None, # layers to unfreeze and train from the original pre-trained model
 )
-model = get_peft_model(model, peft_config)
+
+def get_instructions(user_content, system_content):
+    instructions = [
+        { "role": "system","content": f"{system_content} "},
+    ]
+
+    instructions.append({"role": "user", "content": f"{user_content}"})
+
+    return instructions
+
+def build_llama2_prompt(instructions):
+    stop_token = "</s>"
+    start_token = "<s>"
+    startPrompt = f"{start_token}[INST] "
+    endPrompt = " [/INST]"
+    conversation = []
+    for index, instruction in enumerate(instructions):
+        if instruction["role"] == "system" and index == 0:
+            conversation.append(f"<<SYS>>\n{instruction['content']}\n<</SYS>>\n\n")
+        elif instruction["role"] == "user":
+            conversation.append(instruction["content"].strip())
+        else:
+            conversation.append(f"{endPrompt} {instruction['content'].strip()} {stop_token}{startPrompt}")
+
+    return startPrompt + "".join(conversation) + endPrompt
+
+from trl import SFTTrainer
+import json
+
+def generate_prompt(dataset: list[dict]):
+    processed = []
+    for i in range(len(dataset["question"])):
+        question = dataset["question"][i]
+        answer = dataset["answer"][i]
+        question_answer = {}
+        question_answer['question'] = question
+        question_answer['answer'] = answer
+        question = ""
+        system_content_a = '''
+        You are a to be given questions about REST API Endpoints. Your answers are to be given in code or text.
+        Give no explanation, and only give the answer in the following format, no fluff text:
+        url: <url> headers: <headers> params: <params> body: <body> python code: <code> code needs to be in Python code using the requests library. Do not include imports, just code.
+        '''
+        system_content_b = f'''
+            The input will be a list of strings representing resource paths for a RESTFul API. Give you answers as operations a consumer of the API could perform.
+            Write the operations in natural language, and separate them with a comma, no fluff text.
+        '''
+        system_content_c = f'''
+            The input will be a list of strings representing resource paths for a RESTFul API.
+            Give your response based on which paths require an entity id, and if you can get that entity elsewhere, tell the user how.
+            Write the operations in natural language, and separate them with a comma, no fluff text.
+        '''
+        question = question_answer['question']
+        system_content = ""
+        if 'entity_id' in question:
+            system_content = system_content_c
+        else:
+            system_content = system_content_a
+        system_content_a = '''
+        You are a to be given questions about REST API Endpoints. Your answers are to be given in code or text.
+        Give no explanation, and only give the answer in the following format, no fluff text:
+        url: <url> headers: <headers> params: <params> body: <body> python code: <code> code needs to be in Python code using the requests library. Do not include imports, just code.
+        '''
+        instructions = get_instructions(question, system_content)
+        prompt = build_llama2_prompt(instructions)
+        whole_prompt = f"{prompt} " + f"Answer: {answer}" 
+        processed.append(whole_prompt)
+    return processed
+
+batch_size = 8
+gradient_accumulation_steps = 2
+num_train_epochs = 3
+
+training_params = TrainingArguments(
+    output_dir="./results",
+    num_train_epochs=3,
+    per_device_train_batch_size=batch_size,
+    per_device_eval_batch_size=batch_size//3,
+    gradient_accumulation_steps=gradient_accumulation_steps,
+    gradient_checkpointing=True,
+    optim="paged_adamw_32bit",
+    save_steps=25,
+    logging_steps=25,
+    learning_rate=2e-4,
+    weight_decay=0.001,
+    fp16=True,
+    bf16=False,
+    max_grad_norm=0.3,
+    max_steps=20,
+    warmup_ratio=0.03,
+    group_by_length=False,
+    lr_scheduler_type="constant",
+    report_to="tensorboard"
+)
+
+trainer = SFTTrainer(
+    model=model,
+    train_dataset=train,
+    peft_config=peft_params,
+    max_seq_length=None,
+    tokenizer=tokenizer,
+    args=training_params,
+    formatting_func=generate_prompt,
+    packing=False,
+)
+
+trainer.train()
