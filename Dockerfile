3b4bcd45dceab981f2cb2b1ea41cc20b45c8b5a2
Thomas Knowles <tkk100@msn.com>
2024-08-31 10:15:59 +0100

    Overhaul of Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM nvidia/cuda:11.7.1-cudnn8-devel-ubuntu20.04
+FROM nvidia/cuda:12.5.0-devel-ubuntu22.04
 # Set environment variables
 ENV DEBIAN_FRONTEND=noninteractive
 # Install system dependencies
@@ -20,31 +20,15 @@ WORKDIR /code
 RUN pip install poetry
 COPY ./pyproject.toml ./poetry.lock* /code/
 COPY . /code
+ENV CUDA_HOME=/usr/local/cuda-12.5
+ENV PATH=$PATH:$CUDA_HOME/bin
+ENV LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$CUDA_HOME/lib64
+
 
 RUN poetry config virtualenvs.in-project false
 RUN poetry env use /usr/bin/python3.10
 RUN poetry install
-RUN poetry run pip install torch==2.0.1 torchvision==0.15.2 torchaudio==2.0.2
-RUN apt-get install build-essential g++ clang -y
-RUN poetry run pip install --upgrade pip setuptools wheel
-ENV CUDACXX="/usr/local/cuda-11.7/bin/nvcc"
+
+RUN poetry run pip install torch==2.4.0
+RUN poetry run pip install "unsloth[cu121-torch240] @ git+https://github.com/unslothai/unsloth.git"
 RUN CMAKE_ARGS="-DLLAMA_CUDA=on" FORCE_CMAKE=1 poetry run pip install llama-cpp-python --no-cache-dir --force-reinstall --upgrade
-RUN echo 'export LLAMA_CPP_LIB=~/.cache/pypoetry/virtualenvs/restful-llama-MATOk_fk-py3.10/lib/python3.10/site-packages/llama_cpp/lib/libllama.so' >> ~/.bashrc 
-ENV PATH=/code/.venv/bin:$PATH
-
-RUN poetry run pip install "unsloth[cu121-torch230] @ git+https://github.com/unslothai/unsloth.git"
-
-RUN apt-get update && apt-get install -y openssh-server
-RUN mkdir /var/run/sshd
-
-RUN echo 'root:admin_tk_2024' | chpasswd
-RUN sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config
-RUN apt install nano
-
-# SSH login fix. Otherwise, the user is kicked off after login.
-RUN sed 's@session\s*required\s*pam_loginuid.so@session optional pam_loginuid.so@g' -i /etc/pam.d/sshd
-
-ENV NOTVISIBLE "in users profile"
-RUN echo "export VISIBLE=now" >> /etc/profile
-EXPOSE 22
-CMD ["/usr/sbin/sshd", "-D"]
--- main.py
+++ main.py
@@ -7,15 +7,16 @@ import re
 import sys
 import time
 import uuid
+from typing import Any
 
 import colorama
 import llama_cpp
 import requests
 import structlog
 from colorama import Fore
+from huggingface_hub import hf_hub_download
 from llama_cpp import Llama
 
-from code_executor import execute_llm_code
 from data_generation.schema_processor import (
     APIInfo,
     _generate_full_path_context,
@@ -26,32 +27,36 @@ from data_generation.schema_processor import (
 
 logging.basicConfig(level=logging.CRITICAL)
 
-logger = structlog.get_logger(__name__)
-
 repo_name = "the1ullneversee"
 model_name = "RestfulLlama-8B-Instruct"
+file_name = "restful_llama_8_Q8.gguf"
 colorama.init()
 
+max_seq_length = 2048  # Supports RoPE Scaling interally, so choose any!
 llm_ctx_window = 8096
 # 10% less than the max context window
 llm_ctx_window_warning = llm_ctx_window - (llm_ctx_window * 0.1)
 
 
-def download_model(repo_name: str, model_name: str) -> None:
-    model_url = f"https://huggingface.co/{repo_name}/{model_name}/resolve/main/restful_llama_8_Q8.gguf"
-    model_local_path = "restful_llama_8_Q8.gguf"
+def download_gguf_model(local_dir="./models"):
     try:
-        response = requests.get(model_url)
-        # Raise an exception for bad status codes
-        response.raise_for_status()
-        # Save the JSON data to a file
-        with open(model_local_path, "wb") as f:
-            f.write(response.content)
-        logger.info(f"Model downloaded successfully to restful_llama_8_Q8.gguf")
-    except requests.exceptions.RequestException as e:
-        logger.error(f"Error downloading model: {e}")
+        # Create the local directory if it doesn't exist
+        os.makedirs(local_dir, exist_ok=True)
 
-    return model_local_path
+        # Download the file
+        model_path = hf_hub_download(
+            repo_id=repo_name + "/" + model_name,
+            filename=file_name,
+            local_dir=local_dir,
+            local_dir_use_symlinks=False,
+        )
+
+        print(f"Model downloaded successfully to: {model_path}")
+        return model_path
+
+    except Exception as e:
+        print(f"An error occurred while downloading the model: {e}")
+        return None
 
 
 def load_code_model():
@@ -67,10 +72,35 @@ def load_code_model():
     )
 
 
-async def load_language_model() -> Llama:
+async def load_sloth_model():
+    model, tokenizer = FastLanguageModel.from_pretrained(
+        model_name="the1ullneversee/RestfulLlama-8B-Instruct",
+        # model_name="unsloth/Phi-3-mini-4k-instruct",
+        max_seq_length=llm_ctx_window,
+        dtype=None,
+        load_in_4bit=False,
+    )
+    FastLanguageModel.for_inference(model)  # Enable native 2x faster inference
+    text_streamer = TextStreamer(tokenizer)
+    prompt_llm(model, tokenizer, "Hello, how are you doing?", [])
+    # inputs = tokenizer(
+    #     "Hello, how are you doing?", return_tensors="pt", padding=True
+    # ).to("cuda")
+    # outputs = model.generate(**inputs, max_new_tokens=64)
+    # input_length = inputs["input_ids"].shape[1]
+    # predictions = tokenizer.batch_decode(
+    #     outputs[:, input_length:], skip_special_tokens=True
+    # )
+    # stream_output(predictions[0], Fore.CYAN)
+    return model, tokenizer
+
+
+async def load_language_model() -> None:
     def _load_model():
+        model_path = download_gguf_model()
         return Llama(
-            model_path="./models/restful_llama_8_Q8.gguf",
+            model_path=model_path,
+            # model_path="./models/restful_llama_8_Q8.gguf",
             n_threads=8,  # CPU cores
             n_batch=512,  # Should be between 1 and n_ctx, consider the amount of VRAM in your GPU.
             n_gpu_layers=-1,  # Change this value based on your model and your GPU VRAM pool.
@@ -110,7 +140,7 @@ def download_docs(url: str) -> str:
         tmp_file = "./temp/" + str(uuid.uuid4()) + ".json"
         with open(tmp_file, "w", encoding="utf-8") as f:
             f.write(response.text)
-        logger.info(f"API Docs downloaded successfully to {tmp_file}")
+        stream_output(f"API Docs downloaded successfully to {tmp_file}", Fore.CYAN)
         return tmp_file
     except requests.exceptions.RequestException as e:
         logger.error(f"Error downloading API Docs: {e}")
@@ -122,6 +152,33 @@ further_text_blocks = [
     "RestfulLlama was trained on a dataset of synthetic conversations between users and RESTFul APIs.",
     "Llama 7 70B Instruct was used as the base model for synthetic dataset generation.",
 ]
+
+
+# def prompt_llm(
+#     model, tokenizer: AutoTokenizer, content: str, history: list[dict]
+# ) -> str:
+#     # Prepare the input text
+#     history.append({"role": "system", "content": content})
+#     input_text = format_chat_history(history)
+
+#     # Tokenize the input
+#     inputs = tokenizer(input_text, return_tensors="pt", padding=True).to(model.device)
+#     outputs = model.generate(**inputs, max_new_tokens=64)
+#     input_length = inputs["input_ids"].shape[1]
+#     predictions = tokenizer.batch_decode(
+#         outputs[:, input_length:], skip_special_tokens=True
+#     )
+#     stream_output(predictions[0], Fore.CYAN)
+#     return predictions[0]
+
+
+# def format_chat_history(history: list[dict]) -> str:
+#     formatted_history = ""
+#     for message in history:
+#         role = message["role"]
+#         content = message["content"]
+#         formatted_history += f"{role.capitalize()}: {content}\n"
+#     return formatted_history.strip()
 
 
 def prompt_llm(llm: Llama, content: str, history: list[dict]) -> dict:
@@ -157,7 +214,9 @@ def _api_context_summary(llm: Llama, api_context: APIInfo) -> None:
         )
     history = [{"role": "system", "content": system_message}]
     answer: dict = prompt_llm(llm, system_message, history)
-    answer.get('content').replace("Here is a concise summary of the API in MARKDOWN format:", "")
+    answer.get("content").replace(
+        "Here is a concise summary of the API in MARKDOWN format:", ""
+    )
     history.append(answer)
     stream_output(answer.get("content"), Fore.CYAN, output_cadence=0.01)
 
@@ -248,16 +307,16 @@ async def main() -> None:
 
     llm = model_task.result()
     stream_output("Model loaded successfully!")
-    user_api = "https://petstore.swagger.io/v2/swagger.json"
-    # user_api = input(
-    #     f"Enter the URL of the API Docs you would like to interact with or press enter to chose the default, (default is {default_api}): "
-    # )
-    # if not user_api:
-    #     user_api = default_api
+    default_api = "https://petstore.swagger.io/v2/swagger.json"
+    response = input(
+        f"Enter the URL of the API Docs you would like to interact with or press enter to chose the default, (default is {default_api}): "
+    )
+    if not response:
+        user_api = default_api
     api_docs_file = download_docs(user_api)
     _conversation_loop(llm, api_docs_file)
 
 
 if __name__ == "__main__":
-
+    # asyncio.run(load_sloth_model())
     asyncio.run(main())
